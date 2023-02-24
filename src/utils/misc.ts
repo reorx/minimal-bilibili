e95088d1aec5942ddf9ab51324f70cb097968493
Reorx <novoreorx@gmail.com>
2023-02-24 22:45:39 +0800

    fix datetime display

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -8,6 +8,6 @@ export function formatDuration(seconds: number): string {
 
 export function formatDate(timestamp: number): string {
   const date = new Date(timestamp * 1000);
-  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "2-digit", day: "2-digit" };
-  return new Intl.DateTimeFormat("en-US", options).format(date);
+  const options: Intl.DateTimeFormatOptions = { year: "numeric", month: "2-digit", day: "2-digit", hour: "2-digit", minute: "2-digit" };
+  return new Intl.DateTimeFormat("zh-CN", options).format(date);
 }
